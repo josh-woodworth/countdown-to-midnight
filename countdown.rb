b8419448af5edeb6a16d2fbd56d2bb86329fb203
Josh Woodworth <72992058+josh-woodworth@users.noreply.github.com>
2021-01-16 18:37:47 -0700

    commit tree 81a05d553c97fcdb69298e022be4d9c9a0a55a25

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
+number = 10
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+return "HAPPY NEW YEAR!"
 end
+# sleep = 10 
+
+def countdown_with_sleep(x)
+    sleep 5
+    while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
+
